7b0034fb5a77f0721a7cd14846ddac95302dba5b
nederjair <nederjair@gmail.com>
2025-11-06 22:55:45 +0300

    progress

--- javascript/seminar/code/index.js
+++ javascript/seminar/code/index.js
@@ -1,5 +1,26 @@
-var myheader = document.getElementsByTagName("h1");
-
-myheader[0].addEventListener("click", function () {
-  alert("dont touch me");
+function fillUserWord(userArray, userLetter, word) {
+  for (i = 0; i < word.length; i++) {
+    if (word[i] === userLetter) {
+      userArray[i] = userLetter;
+    }
+  }
+}
+var guessButton = document.getElementById("guess");
+guessButton.addEventListener("click", function (userArray, word) {
+  var userLetter = document.getElementById("userLetter").innerText;
+  //fillUserWord(userArray, userLetter, word);
+  alert(word);
 });
+
+var word = "pizza";
+const spanArray = [];
+const userArray = [];
+var letters = document.getElementById("letters");
+for (i = 0; i < word.length; i++) {
+  var initialText = document.createTextNode("  _____  ");
+  spanArray[i] = document.createElement("span");
+  spanArray[i].setAttribute("id", "letter" + i);
+  spanArray[i].appendChild(initialText);
+  letters.appendChild(spanArray[i]);
+  userArray[i] = "$";
+}
